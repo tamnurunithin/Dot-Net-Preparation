714a0af0112d3edd5a3c0eb12e7af5377fa0c206
Nithin <tamnurunithin@gmail.com>
2025-08-02 11:09:35 +0530

    Deleted Day1__C# Basics folder

--- Day1__C# Basics/example.cs
+++
@@ -1,9 +0,0 @@
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine("Hello " + name);
-    }
-}
--- Day1__C# Basics/notes.md
+++
@@ -1,8 +0,0 @@
-# Day 1: C# Basics
-
-## Topics
-
-- What is C# and .NET?
-- Variables and Data Types
-- Input and Output
-- Operators
