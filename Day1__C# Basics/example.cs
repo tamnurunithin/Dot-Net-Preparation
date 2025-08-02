056bc2591a7081940ed46008a109a8b5a072e450
Nithin <tamnurunithin@gmail.com>
2025-08-02 10:55:37 +0530

    Day_01: C# Basics

---
+++ Day1__C# Basics/example.cs
@@ -0,0 +1,9 @@
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+    }
+}
---
+++ Day1__C# Basics/notes.md
@@ -0,0 +1,8 @@
+# Day 1: C# Basics
+
+## Topics
+
+- What is C# and .NET?
+- Variables and Data Types
+- Input and Output
+- Operators
